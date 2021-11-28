153bb417131c3987cec91fb913b8b3f6f11fe52c
Christopherdu65 <dadadoytembe@gmail.com>
2021-11-27 19:46:38 -0500

    minor fix v4

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import './header.css'
 import { Link } from 'react-router-dom'
+import {useContext} from "react";
+import {Context} from "../../context/Context";
 
 export default function Header() {
+    const {user} = useContext(Context);
+    function redirect(e) {
+        e.preventDefault();
+        user? window.location.replace("/quiz"): window.location.replace("/register");
+    }
+
     return (
         // <div className="main-container">
             <div className="container">
@@ -10,7 +18,8 @@ export default function Header() {
                         <div className="content">
                             <h1 className="heading">Prove your worth to the <span>Goddess</span></h1>
                             <p className="info">Have fun and play our anime quiz as you compete with other knights in the leaderboard. </p>
-                            <Link to="/register"><button className="header-btn">Get Started</button></Link>
+                            {/*<Link to="/register"><button className="header-btn">Get Started</button></Link>*/}
+                            <button onClick={redirect} className="header-btn">Get Started</button>
                         </div>
                     </div>
                 </div>
